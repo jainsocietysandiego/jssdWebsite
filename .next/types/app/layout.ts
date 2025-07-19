45d2ad4545628647678db1d6c59e3c13c1853a21
deepjaindrj <deepgaliya@gmail.com>
2025-07-19 19:14:31 +0530

    pathshala page added

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\FreeLance\NGO_jain\app\layout.tsx
+// File: C:\Users\KEYUR JAIN\Desktop\Playground\JSSD Next\NGO_jain\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
